e8c8abd09463e49d5de54de5b5353c7c4af24925
Arfan Shaikh <arfanshaikh1@gmail.com>
2021-07-15 00:41:14 +0530

    fix .gitignore

--- src/index.js
+++ src/index.js
@@ -1,31 +1,12 @@
-require('./db/mongoose');
-const express = require('express');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
-
-const app = express();
+const app = require('./app');
 const port = process.env.PORT;
-const multer = require('multer');
+
+
+app.listen(port, () =>{
+    console.log(`Listening on port `+port);
+})
 //configuration
-const upload = multer({
-    dest: 'images',
-    limits :{
-        fileSize: 1000000
-    },
-    fileFilter(req, file, callback) {
-        if(!file.originalname.match(/\.(doc|docx)$/)){
-            return callback(new Error('Please upload a word document'))
-        }
-        callback(undefined, true);
-    }
-})
 
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) =>{
-    res.status(400).send({ error: error.message})
-})
 
 
 
@@ -52,15 +33,9 @@ app.post('/upload', upload.single('upload'), (req, res) => {
 
 
 
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 
 
-app.listen(port, () =>{
-    console.log(`Listening on port `+port);
-})
 
 // const Task = require('./models/task');
 // const User = require('./models/user');
